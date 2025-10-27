2ce37392d8c84e1f6e85c100b3dffcaff3256a75
TLAllen662 <tlallen1419@gmail.com>
2025-10-26 20:33:05 -0500

    define get_request and post_request

--- http_requests.py
+++ http_requests.py
@@ -5,3 +5,17 @@ import requests
 class JSONPlaceholder:
     def __init__(self, base_url="https://jsonplaceholder.typicode.com"):
         self.base_url = base_url
+    def get_request(self):
+        response = requests.get(self.base_url)
+        return {
+            "status_code": response.status_code,
+            "headers": response.headers,
+            "content": response.content
+        }
+    def post_request(self, data):
+        response = requests.post(self.base_url, json=data)
+        return {
+            "status_code": response.status_code,
+            "headers": response.headers,
+            "content": response.content
+        }
