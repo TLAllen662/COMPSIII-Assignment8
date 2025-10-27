504a5acc09ea303e1596af62d95c839fff180e9a
TLAllen662 <tlallen1419@gmail.com>
2025-10-26 20:44:22 -0500

    create put_request and delete_request

--- http_requests.py
+++ http_requests.py
@@ -1,10 +1,11 @@
 # Write your code here
 import requests
 
-
+#create JSONPlaceholder class
 class JSONPlaceholder:
     def __init__(self, base_url="https://jsonplaceholder.typicode.com"):
         self.base_url = base_url
+#define get_request
     def get_request(self):
         response = requests.get(self.base_url)
         return {
@@ -12,8 +13,29 @@ class JSONPlaceholder:
             "headers": response.headers,
             "content": response.content
         }
+#define post_request
     def post_request(self, data):
         response = requests.post(self.base_url, json=data)
+        return {
+            "status_code": response.status_code,
+            "headers": response.headers,
+            "content": response.content
+        }
+#create put_request
+    def put_request(self, data):
+        response = requests.put(self.base_url, json=data)
+        return {
+            "status_code": response.status_code,
+            "headers": response.headers,
+            "content": response.content
+        }
+    data = {
+        "title" = "TITLE_DATA"
+        "body" = "BODY_DATA"
+    }
+#create delete_request
+    def delete_request(self):
+        response = requests.delete(self.base_url)
         return {
             "status_code": response.status_code,
             "headers": response.headers,
